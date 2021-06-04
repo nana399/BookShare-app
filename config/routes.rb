34723b7c5af6bc844bee729b152cff76d0ab54be
nana399 <kinakorabbit1182@gmail.com>
2021-06-04 20:55:40 +0900

    root位置を変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   get 'books/booksearch', to: 'books#new'
+  root 'books#index'
   resources :books do
     resources :likes, only: [:create, :destroy]
-    root 'books#index'
   end
  
 end
