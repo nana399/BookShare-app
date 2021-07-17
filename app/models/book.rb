dda2297c266732fe336193ce9beb2355adfdada6
nana399 <kinakorabbit1182@gmail.com>
2021-07-17 13:11:36 +0900

    【Add】画像取得のコードを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   belongs_to :user, optional: true
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-  mount_uploader :image, ImageUploader
+  
 end
